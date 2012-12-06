2844fede8e1afb8820fc4ca53d18baca9b1064b6
GenGoGo <erotari@gmail.com>
2012-12-06 19:34:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseDepotnApp::Application.config.secret_token = '3cbeb33acbe6d84e9cce81f8cbc9d8a49e236a282d652fc71f6abe73a09511e58cf31b25ffbd3a7336a68e2a9c95a3d99ecf7c8ad0639c287b6d22c17a72af18'
