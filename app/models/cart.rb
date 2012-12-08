ae29a2d1feef0ec670035d9dcc693dd05edc2360
GenGoGo <erotari@gmail.com>
2012-12-08 11:58:23 +0200

    any line item check

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,3 @@
 class Cart < ActiveRecord::Base
-	has_many :line_item
+	has_many :line_item, :dependent => destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,16 @@ class Product < ActiveRecord::Base
 	validates :title, :uniqueness => true
 	validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
 	validates :image_url, :format => { :with => %r{\.(gif|jpg|png)$}i, :message => "Must be JPG, PNG or GIF" }
+	
+	has_many :line_items
+	before_destroy :ensure_not_referenced_any_line_item
+	
+	def ensure_not_referenced_any_line_item
+		if line_items.empty?
+			true
+		else
+			errors.add(:base, 'There ate line itms')
+			false
+		end
+	end
 end
