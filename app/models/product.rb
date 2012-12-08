446f131de6694c4841167cec6f0db7c8c4c86b63
GenGoGo <erotari@gmail.com>
2012-12-08 11:05:23 +0200

    Added some UI

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	default_scope :order => 'price'
-	scope :cheap, where('price < 60')
+	scope :cheap, where('price < 600')
 	validates :title, :description, :image_url, :price, :presence => true
 	validates :title, :uniqueness => true
 	validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
