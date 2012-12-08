0fa47a5929d9ca74e8107fb9560071d3c3784f3e
GenGoGo <erotari@gmail.com>
2012-12-08 12:49:52 +0200

    Added cart button and helper cart link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,10 @@ class ApplicationController < ActionController::Base
   
   private
 	def current_cart
-		begin
 		Cart.find(session[:cart_id])
 		rescue ActiveRecord::RecordNotFound
 			cart = Cart.create
 			session[:cart_id] = cart.id
 			cart
 		end
-	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def current_cart_link
+		if session[:cart_id]
+			#cart_id = current_cart
+			link_to "Your cart", cart_path(Cart.find(session[:cart_id]))
+		else
+			" "
+		end
+	end
 end
